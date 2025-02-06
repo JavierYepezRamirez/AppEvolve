48d380128881252c1a272e841940e74c5a1ce2b4
Javier Yepez <javier_yepez@outlook.com>
2025-02-06 17:10:10 -0600

    Login, solo falta mensaje de error, funcionamento correcto

--- app/src/main/java/com/cinergia/appevolve/Login/LoginActivity.kt
+++ app/src/main/java/com/cinergia/appevolve/Login/LoginActivity.kt
@@ -1,7 +1,10 @@
 package com.cinergia.appevolve.Login
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.InputType
+import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -23,10 +26,11 @@ class LoginActivity : AppCompatActivity() {
         }
 
         val tvBienvenida = findViewById<TextView>(R.id.tvBienvenida)
-        val usuario: String = intent.extras?.getString("EXTRA_USUARIO").orEmpty()
         val etContrasena = findViewById<AppCompatEditText>(R.id.etContrasena)
         val btnTogglePassword = findViewById<ImageView>(R.id.btnTogglePassword)
+        val btnEntrar = findViewById<Button>(R.id.btnEntrar)
 
+        val usuario: String = intent.extras?.getString("EXTRA_USUARIO").orEmpty()
         tvBienvenida.text = "Bienvenido $usuario"
 
         btnTogglePassword.setOnClickListener {
@@ -40,5 +44,37 @@ class LoginActivity : AppCompatActivity() {
             etContrasena.setSelection(etContrasena.text?.length ?: 0)
         }
 
+        btnEntrar.setOnClickListener {
+            val contrasena = etContrasena.text.toString()
+
+            when (usuario) {
+                "Usuario1" -> navigateInicioUsuario1(usuario, contrasena)
+                "Usuario2" -> navigateInicioUsuario2(usuario, contrasena)
+            }
+        }
+    }
+
+    private fun navigateInicioUsuario1(usuario: String, contrasena: String) {
+        if (contrasena == "123456") {
+            Log.e("MiTag", "Contraseña correcta para $usuario")
+            //val intent = Intent(this,MainActivity::class.java)
+            intent.putExtra("EXTRA_USUARIO",usuario)
+            startActivity(intent)
+        } else {
+            Log.e("MiTag", "Error en contraseña de $usuario")
+            //Mostrar un mensaje de error
+        }
+    }
+
+    private fun navigateInicioUsuario2(usuario: String, contrasena: String) {
+        if (contrasena == "654321") {
+            Log.e("MiTag", "Contraseña correcta para $usuario")
+            //val intent = Intent(this,MainActivity::class.java)
+            intent.putExtra("EXTRA_USUARIO",usuario)
+            startActivity(intent)
+        } else {
+            Log.e("MiTag", "Error en contraseña de $usuario")
+            //Mostrar un mensaje de error
+        }
     }
 }
